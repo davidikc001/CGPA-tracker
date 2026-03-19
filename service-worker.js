94468b8e9cc5edc597374be92e24368949c054d4
davidikc001 <davidikc456@gmail.com>
2026-03-19 11:15:09 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,21 @@
-const cacheName = 'cgpa-tracker-v1';
-const filesToCache = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/manifest.json',
-  '/service-worker.js',
-  '/favicon.ico'
+const CACHE_NAME = "cgpa-app-v1";
+
+const urlsToCache = [
+  "index.html",
+  "cgpa.css",
+  "app.js"
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== cacheName ? caches.delete(key) : null))
-    )
-  );
-});
-
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
   );
 });
